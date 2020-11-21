624d0c2652f4dee6184736db54adba3d3beb8442
kontrolbits <gabodecampos@gmail.com>
2020-11-21 15:14:58 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Navbar msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
